7ec55b4a04b4291292cbf72271cdf3e59e0911f1
maxim-f1 <maxconal228@gmail.com>
2024-11-22 23:20:42 +0300

    Add endpoint for parameters limits

--- backend/src/api/fishes/router.py
+++ backend/src/api/fishes/router.py
@@ -2,9 +2,9 @@ from typing import List
 
 from fastapi import APIRouter, status, Body
 
-from src.domain.fish.dto import FishCreateResponseDTO, FishCreateDTO
+from src.domain.fish.dto import FishCreateResponseDTO, FishCreateDTO, FishParametersLimitsDTO
 from src.domain.fish.exception import FISH_ID_OVERLAP_EXCEPTION
-from src.domain.fish.service import create_fishes
+from src.domain.fish.service import create_fishes, get_fishes_parameters_limits
 from src.utils.docs_utils import build_exception_responses
 
 fishes_rest_v1 = APIRouter(
@@ -25,3 +25,12 @@ async def create_fishes_endpoint(
         body: List[FishCreateDTO] = Body(...)
 ) -> List[FishCreateResponseDTO]:
     return await create_fishes(body)
+
+
+@fishes_rest_v1.get(
+    path='/parameters-limits',
+    status_code=status.HTTP_200_OK,
+    response_model=FishParametersLimitsDTO,
+)
+async def get_fishes_parameters_limits_endpoint() -> List[FishCreateResponseDTO]:
+    return await get_fishes_parameters_limits()
--- backend/src/domain/fish/dto.py
+++ backend/src/domain/fish/dto.py
@@ -1,23 +1,43 @@
 import uuid
 
-from pydantic import Field
+from pydantic import Field, field_validator
 
 from src.domain.abc.dto import ABCDTO
 
 
 class FishCreateDTO(ABCDTO):
-    id: str | None = Field(..., default_factory=lambda: str(uuid.uuid4()))
+    id: str | None = Field(...)
     weight: float | None = Field(None, ge=0)
     length: float | None = Field(None, ge=0)
     thickness: float | None = Field(None, ge=0)
     eggs_weight: float | None = Field(None, ge=0)
     egg_weight: float | None = Field(None, ge=0)
 
+    @field_validator('id')
+    @classmethod
+    def id_validator(cls, v: str | None) -> str:
+        if v is None:
+            return str(uuid.uuid4())
+        return v
+
 
 class FishCreateResponseDTO(ABCDTO):
-    id: str
+    id: str = Field(...)
     weight: float | None = Field(None)
     length: float | None = Field(None)
     thickness: float | None = Field(None)
     eggs_weight: float | None = Field(None)
     egg_weight: float | None = Field(None)
+
+
+class ParameterLimitDTO(ABCDTO):
+    min: float | None = Field(None)
+    max: float | None = Field(None)
+
+
+class FishParametersLimitsDTO(ABCDTO):
+    weight: ParameterLimitDTO
+    length: ParameterLimitDTO
+    thickness: ParameterLimitDTO
+    eggs_weight: ParameterLimitDTO
+    egg_weight: ParameterLimitDTO
