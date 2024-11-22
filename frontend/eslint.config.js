809b71bc9464ce1ef2e5db696613c73cf3d96b5a
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2024-11-22 20:35:54 +0400

    rep init

---
+++ frontend/eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config({
+  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  files: ['**/*.{ts,tsx}'],
+  ignores: ['dist'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+  },
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/prop-types': 'off',
+    'semi': ['error', 'always', { 'omitLastInOneLineBlock': false }],
+    'comma-dangle': ['error', 'never'],
+    quotes: ['error', 'single'],
+    'indent': ['error', 'tab'],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        "args": "all",
+        "argsIgnorePattern": "^_",
+        "caughtErrors": "all",
+        "caughtErrorsIgnorePattern": "^_",
+        "destructuredArrayIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "ignoreRestSiblings": true
+      }
+    ]
+  },
+})
---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+	return (
+		<h1>AgroCode-hackathon-2024</h1>
+	);
+}
+
+export default App;
