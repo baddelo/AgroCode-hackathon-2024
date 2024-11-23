cff59fdc947ac397475048323473e8d218a6c682
Zlyden-1 <Zlyden-1@yandex.ru>
2024-11-23 17:24:02 +0400

    feat: excel mock

---
+++ backend/src/api/excel/router.py
@@ -0,0 +1,33 @@
+import io
+from typing import List, Annotated
+
+from fastapi import APIRouter, status, Body, Query, File
+from starlette.responses import StreamingResponse
+
+excel_rest_v1 = APIRouter(
+    tags=["Excel"],
+    prefix='/excel'
+)
+
+
+@excel_rest_v1.post(
+    '/upload',
+    status_code=status.HTTP_201_CREATED
+)
+async def upload_excel(
+    file: Annotated[bytes, File()]
+) -> None:
+    return
+
+
+@excel_rest_v1.get(
+    '/download',
+)
+async def download_excel() -> StreamingResponse:
+    with open('test.xlsx', 'rb') as f:
+        headers = {
+            'Content-Disposition': 'attachment; filename="name_of_excel_file.xls"',
+        }
+        return StreamingResponse(
+            io.BytesIO(f.read()), headers=headers
+        )
--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, APIRouter
 from starlette.middleware.cors import CORSMiddleware
 
+from src.api.excel.router import excel_rest_v1
 from src.config.backend import BACKEND_CONFIG
 from src.database.mongo.model import init_models
 from src.utils.routers_utils import include_routers
@@ -21,6 +22,7 @@ async def lifespan(app_: FastAPI):
     v1_routers = [
         fishes_rest_v1,
         groups_rest_v1,
+        excel_rest_v1,
     ]
     v1_router = include_routers(APIRouter(prefix='/v1'), v1_routers)
     main_router = include_routers(APIRouter(prefix='/api'), (v1_router,))
