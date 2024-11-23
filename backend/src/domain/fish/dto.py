093c526046891b7789bea7f9ecf0bd0b94088d9c
maxim-f1 <maxconal228@gmail.com>
2024-11-23 17:23:51 +0300

    Add calculation fields

--- backend/src/domain/fish/dto.py
+++ backend/src/domain/fish/dto.py
@@ -1,7 +1,7 @@
 import uuid
-from typing import Literal
+from typing import Literal, Self
 
-from pydantic import Field, field_validator
+from pydantic import Field, field_validator, model_validator
 
 from src.domain.abc.dto import ABCDTO
 
@@ -68,12 +68,47 @@ class FishParametersLimitsDTO(ABCDTO):
 
 class FishGetDTO(ABCDTO):
     id: str = Field(...)
-    height: float | None = Field(None)
     weight: float | None = Field(None)
     length: float | None = Field(None)
+    height: float | None = Field(None)
     thickness: float | None = Field(None)
     eggs_weight: float | None = Field(None)
     egg_weight: float | None = Field(None)
+
+    k_upit: float | None = Field(None)
+    i_tolsh: float | None = Field(None)
+    i_visots: float | None = Field(None)
+    dolya_icry: float | None = Field(None)
+    work_plodovitost: float | None = Field(None)
+    otnosit_plodovitost: float | None = Field(None)
+    index_reproduction: float | None = Field(None)
+
+    @model_validator(mode='after')
+    def after_validator(self) -> Self:
+        if self.weight is not None and self.length is not None and self.length != 0:
+            self.k_upit = round(self.weight / pow(self.length, 3) * 100, 4)
+
+        if self.thickness is not None and self.length is not None and self.length != 0:
+            self.i_tolsh = round(self.thickness / self.length * 100, 4)
+
+        if self.height is not None and self.length is not None and self.length != 0:
+            self.i_visots = round(self.height / self.length * 100, 4)
+
+        if self.eggs_weight is not None and self.weight is not None and self.weight != 0:
+            self.dolya_icry = round(self.eggs_weight / self.weight * 100, 4)
+
+        if self.eggs_weight is not None and self.egg_weight is not None and self.egg_weight != 0:
+            self.work_plodovitost = round(self.eggs_weight / self.egg_weight, 4)
+
+        if self.work_plodovitost is not None and self.weight is not None and self.eggs_weight is not None:
+            if ((self.weight - self.eggs_weight) / 1000) != 0:
+                self.otnosit_plodovitost = round((self.work_plodovitost * 1000) / ((self.weight - self.eggs_weight) / 1000), 4)
+
+        if self.eggs_weight is not None and self.weight is not None and self.eggs_weight is not None:
+            if ((self.weight - self.eggs_weight) / 1000) != 0:
+                self.index_reproduction = round(self.eggs_weight / ((self.weight - self.eggs_weight) / 1000), 4)
+
+        return self
 
 
 class OrdersDTO(ABCDTO):
